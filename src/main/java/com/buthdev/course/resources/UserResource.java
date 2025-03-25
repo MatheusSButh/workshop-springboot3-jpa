8e10d4bca6afd5ac9b1161534aa8b4930306a6fc
Matheus <matheusdsouzabuth@gmail.com>
2025-03-25 09:55:26 -0300

    User entity and resource

---
+++ src/main/java/com/buthdev/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.buthdev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buthdev.course.entitites.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "61-98408-2991", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
